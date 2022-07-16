99446cf4c25eb0b3621076059c24aa0a80813872
FlameDavid92 <davidefiguccia2020@outlook.it>
2022-07-16 13:19:30 +0200

    2 files modified, 1 added

--- src/app/alerts-sample/alerts-sample.component.ts
+++ src/app/alerts-sample/alerts-sample.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-alerts-sample',
   templateUrl: './alerts-sample.component.html',
+  styleUrls: ['./alerts-sample.component.css'],
 })
 export class AlertsSampleComponent {}
